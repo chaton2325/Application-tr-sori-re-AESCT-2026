f94fbb486c7422916ede66fdf4c6f6140707d387
Tsari Sila <developpement0123@gmail.com>
2026-06-23 22:47:35 +0100

    add change password and replace euro to tnd

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+from app.models.user import User
+from flask_login import current_user
 
 class LoginForm(FlaskForm):
     username = StringField('Nom d\'utilisateur', validators=[DataRequired()])
@@ -15,3 +17,13 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirmer le mot de passe', validators=[DataRequired(), EqualTo('password')])
     role_id = SelectField('Rôle', coerce=int, validators=[DataRequired()])
     submit = SubmitField('Enregistrer')
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Ancien mot de passe', validators=[DataRequired()])
+    new_password = PasswordField('Nouveau mot de passe', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmer le nouveau mot de passe', validators=[DataRequired(), EqualTo('new_password')])
+    submit = SubmitField('Changer le mot de passe')
+
+    def validate_old_password(self, old_password):
+        if not current_user.check_password(old_password.data):
+            raise ValidationError('L\'ancien mot de passe est incorrect.')
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app.models.user import User
-from app.forms.auth import LoginForm
+from app.forms.auth import LoginForm, ChangePasswordForm
 from app.utils.decorators import log_action
+from app import db
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -31,3 +32,15 @@ def logout():
     log_action('Déconnexion')
     logout_user()
     return redirect(url_for('auth.login'))
+
+@auth_bp.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        current_user.set_password(form.new_password.data)
+        db.session.commit()
+        log_action('Changement de mot de passe')
+        flash('Votre mot de passe a été mis à jour.', 'success')
+        return redirect(url_for('main.index'))
+    return render_template('auth/change_password.html', title='Changer le mot de passe', form=form)
